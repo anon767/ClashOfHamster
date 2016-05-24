03784172bc6b81bb159fd5edf1c47390b5be3b8f
Tom <tomganzka@gmail.com>
2016-05-24 16:54:16 +0200

    caching playerinfo

--- client/playerInfo.js
+++ client/playerInfo.js
@@ -1,4 +1,4 @@
-/* global createjs */
+/* global createjs, queue */
 
 //player status info
 var PlayerInfo = function () {
@@ -6,11 +6,12 @@ var PlayerInfo = function () {
     this.create = function (stage) {
         var img = new Image(250, 114);
         img.src = queue.getResult("playerInfo").src;
-        this.canvasO =  new createjs.Shape();
+        this.canvasO = new createjs.Shape();
         this.canvasO.x = 0;
         this.canvasO.y = 0;
         this.canvasO.tickEnabled = false;
         this.canvasO.graphics.beginBitmapFill(img, 'no-repeat').drawRect(stage.x, 0, 250, 114);
+        this.canvasO.cache(-250, -114, 250 * 2, 114 * 2); //cache this shit, unless its moveable later we dont have to update cache
         stage.addChild(this.canvasO);
         stage.nonBlocking[this.canvasO.id] = this.canvasO;
         return this.canvasO;
